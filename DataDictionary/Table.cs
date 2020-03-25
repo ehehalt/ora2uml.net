67064d51222e20a32aa00ffe7ac3cbd2a4a7f478
Mike <mike42@hotmail.de>
2020-03-25 07:43:40 +0100

    Table + Column

--- DataDictionary/Table.cs
+++ DataDictionary/Table.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace Ora2Uml.DataDictionary
 {
     public class Table {
         public String Schema { get; set; }  
-        public String TableName { get; set; }
+        public String Name { get; set; }
+
+        public IList<Column> Columns { get; protected set; } = new List<Column>();
     }
 }
