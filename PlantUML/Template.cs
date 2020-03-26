fb9ac83f08656895d4caa3467a02e9cf947329d5
Mike <mike42@hotmail.de>
2020-03-26 19:53:07 +0100

    Template started

---
+++ PlantUML/Template.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Ora2Uml.Objects;
+
+namespace Ora2Uml.PlantUML
+{
+    public static class Template
+    {
+        #region Constants
+
+        private const String templatePrefix = @"
+@startuml
+!define primary_key(x) <b>x</b>
+!define unique(x) <color:green>x</color>
+!define not_null(x) <u>x</u>
+
+hide methods
+hide stereotypes
+
+";
+
+        private const String templatePostfix = @"
+
+@enduml";
+
+        #endregion Constants
+
+        public static String GeneratePlantUML(IList<Table> tables)
+        {
+            var uml = "";
+
+            foreach(Table table in tables)
+            {
+                var tbl = $"Table({table.TableName}, \"{table.TableName}\n(Description)\") {{\n\n";
+
+                uml += tbl;
+            }
+
+            uml = templatePrefix + uml + templatePostfix;
+
+            return uml;
+        }
+
+    }
+}
